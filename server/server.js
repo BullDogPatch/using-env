10ffde5edc5a522c37f21afd8bff50b6ccccce58
Craig Clayton <craig__clayton@outlook.com>
2025-01-08 11:40:16 +0000

    add two endpoints, basics are working

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('<h1>This is the root route!</h1>');
+});
+
+app.get('/puppy-images', async (req, res) => {
+  const API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=puppy`;
+  const response = await fetch(API);
+  const data = await response.json();
+
+  res.json(data.results);
+});
+
+app.get('/cat-images', async (req, res) => {
+  const API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=cat`;
+  const response = await fetch(API);
+  const data = await response.json();
+
+  res.json(data.results);
+});
+
+app.listen(8080, () => {
+  console.log(`Server running on port 8080`);
+});
